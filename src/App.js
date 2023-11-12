4a6f6a0a4d0e2f4aa778d10ae03fce3458a24bdb
Bao Nguyen <bnguyen@colgate.edu>
2023-11-12 23:02:22 +0700

    Initial commit, set up basics

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
-import logo from './logo.svg';
+import React, {Component} from 'react';
+import logo from './spotify.svg';
+import {authURL, preservedState} from "./authorize";
+import {checkState, access_token} from "./processToken";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor (){
+      super();
+      this.state = {
+          token: null,
+          top_artist: "",
+          artist_image: "",
+      };
+      this.getTopArtist = this.getTopArtist.bind(this);
+      this.checkToken = this.checkToken.bind(this);
+  }
+  componentDidMount(){
+    if(access_token){
+        this.setState({
+            token: access_token
+        });
+        this.getTopArtist(access_token);
+    }
+    this.interval = setInterval(() => this.checkToken(), 3000);
+  }
+  checkToken(){
+    if(this.state.token){
+        this.getTopArtist(this.state.token);
+    }
+  }
+  componentWillUnmount(){
+    clearInterval(this.interval);
+  }
+  getTopArtist(token){
+
+  }
+  render (){
+    return (
+        <div className="App">
+            <header className="App-header">
+            {!this.state.token && (
+                <div className="Intro">
+                    <img src={logo} className="App-logo" alt="logo" />
+                    <a className = "log-in-spotify"
+                        href = {`${authURL}`}>
+                            Sign in to Spotify
+                    </a>
+                    <p>
+                        Let's get to know your top artist!
+                    </p>
+                </div>
+            )}
+            {this.state.token && (
+                <div className="Final">
+                    <h1>And your top artist is: </h1>
+                    <h2>{this.state.top_artist} </h2>
+                    <img src={`${this.state.artist_image}`} alt = "Artist" />
+                </div>
+            )}
+            </header>
+        </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/authorize.js
@@ -0,0 +1,19 @@
+function generateRandomString(length) {
+     var text = '';
+    var possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+     for (var i = 0; i < length; i++) {
+          text += possible.charAt(Math.floor(Math.random() * possible.length));
+     }
+    return text;
+}
+var state = generateRandomString(16);
+export const authURL = 'https://accounts.spotify.com/authorize'
++ '?response_type=token'
++ '&client_id=' + encodeURIComponent('36c8349b1f514b8a8c296ba935d44df6')
++ '&scope=' + encodeURIComponent('user-top-read')
++ '&redirect_uri=' + encodeURIComponent('http://localhost:3000')
++ '&state=' + encodeURIComponent(state);
+
+export const preservedState = state;
+
+
